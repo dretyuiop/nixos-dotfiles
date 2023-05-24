af995e6a63587891cfdb339b6428c815c4cc97b8
cch <81854406+dretyuiop@users.noreply.github.com>
2023-05-24 23:14:07 +0800

    removed modified tela-dracula-dark and used symlinks with icon setup file

--- run_once_after_setup-icons.sh
+++ run_once_after_setup-icons.sh
@@ -5,3 +5,6 @@ rm /home/cch/.local/share/icons/Tela-dracula/*/panel/steam_tray_mono.svg
 
 #lutris icon
 ln -sf ~/.local/icons/lutris.svg ~/.local/share/icons/Tela-dracula/scalable/apps/lutris.svg
+
+#executable icon
+ln -sf ~/.local/icons/application-x-executable.svg ~/.local/share/icons/Tela-dracula/scalable/mimetypes/application-x-executable.svg
