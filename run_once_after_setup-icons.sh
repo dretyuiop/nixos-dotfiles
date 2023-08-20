6950913381632fe7f2aca0e3225d0d8c56bc372c
cch <chewch03@gmail.com>
2023-08-20 23:29:33 +0800

    add icons

--- run_once_after_setup-icons.sh
+++ run_once_after_setup-icons.sh
@@ -7,7 +7,7 @@ rm /home/cch/.local/share/icons/Tela-dracula/*/panel/steam_tray_mono.svg
 ln -sf ~/.local/icons/lutris.svg ~/.local/share/icons/Tela-dracula/scalable/apps/lutris.svg
 
 #executable icon
-ln -sf ~/.local/icons/application-x-executable.svg ~/.local/share/icons/Tela-dracula/scalable/mimetypes/application-x-executable.svg
+ln -sf ~/.local/icons/application-default-icon.svg ~/.local/share/icons/Tela-dracula/scalable/mimetypes/application-x-executable.svg
 
 #qbittorent icon
 ln -sf ~/.local/icons/qbittorrent.svg ~/.local/share/icons/Tela-dracula/scalable/apps/qbittorrent.svg
