1cf443fa0980ea6ee47320ee3115c0249fe956ed
cch <chewch03@gmail.com>
2023-08-22 00:50:03 +0800

    encrypt scripts

--- dot_local/bin/executable_setup-icons.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#steam tray icon
-rm /home/cch/.local/share/icons/Tela-dracula/*/panel/steam_tray_mono.svg
-
-#lutris icon
-ln -sf ~/.local/icons/lutris.svg ~/.local/share/icons/Tela-dracula/scalable/apps/lutris.svg
