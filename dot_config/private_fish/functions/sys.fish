0d58968f8b9a7baec8ca0021db861b6931c44cb9
cch <chewch03@gmail.com>
2023-12-06 00:07:19 +0800

    update

--- dot_config/private_fish/functions/sys.fish
+++ dot_config/private_fish/functions/sys.fish
@@ -1,4 +1,4 @@
-function sys --wraps=systemctl --description 'alias sys systemctl'
-  systemctl $argv
+function sys --wraps=systemctl --wraps='sudo systemctl' --description 'alias sys=sudo systemctl'
+  sudo systemctl $argv
         
 end
